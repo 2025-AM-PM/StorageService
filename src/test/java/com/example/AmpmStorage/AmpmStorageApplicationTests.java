251a69e9f6f1ca34353a2104bc006d21b4984c31
badapodo <ksg6736@gmail.com>
2025-09-01 19:42:54 +0900

    feat: init

---
+++ src/test/java/com/example/AmpmStorage/AmpmStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmpmStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmpmStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
