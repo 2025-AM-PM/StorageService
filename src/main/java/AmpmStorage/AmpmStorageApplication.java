1057ef7cad4e7a2070029974420e821cb0f17c2a
badapodo <ksg6736@gmail.com>
2025-09-01 20:00:41 +0900

    refactor: 디렉터리 구조 변경

--- src/main/java/AmpmStorage/AmpmStorageApplication.java
+++ src/main/java/AmpmStorage/AmpmStorageApplication.java
@@ -1,4 +1,4 @@
-package com.example.AmpmStorage;
+package AmpmStorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/AmpmStorage/ServletInitializer.java
+++ src/main/java/AmpmStorage/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.AmpmStorage;
+package AmpmStorage;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
