9603f00f90bf2e34e3cc1dd8f5ae44bd81310e0f
badapodo <ksg6736@gmail.com>
2025-11-13 09:33:45 +0900

    feat: 프론트엔드 주소 CORS 정책 추가

--- src/main/java/AmpmStorage/common/config/WebConfig.java
+++ src/main/java/AmpmStorage/common/config/WebConfig.java
@@ -2,15 +2,22 @@ package AmpmStorage.common.config;
 
 import AmpmStorage.common.interceptor.SignatureVerificationInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
     private final SignatureVerificationInterceptor signatureVerificationInterceptor;
+    private final String frontendDomain;
+
+    public WebConfig(SignatureVerificationInterceptor signatureVerificationInterceptor,
+                     @Value("${frontend.domain}") String frontendDomain) {
+        this.signatureVerificationInterceptor = signatureVerificationInterceptor;
+        this.frontendDomain = frontendDomain;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -22,7 +29,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("null", "http://localhost:3000") // "null"과 "http://localhost:3000" Origin 허용
+                .allowedOrigins("null", frontendDomain)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
